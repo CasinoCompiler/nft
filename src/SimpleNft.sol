543401659263870f993fea18275355528d4cb618
CC <casinocompiler@gmail.com>
2024-09-06 22:34:27 +0100

    initial commit: Foundry project setup

---
+++ src/SimpleNft.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+/**
+ * @title Simple NFT contract
+ * @author CC
+ * @notice 
+ * @dev 
+ */
+
+/** Imports */
+// @Order Imports, Interfaces, Libraries, Contracts
+
+/** Errors */
+
+contract ContractName {
+
+    /** Type Declarations */
+
+    /** State Variables */
+
+    /** Events */
+
+    /** Constructor */
+
+    /** Modifiers */
+
+    /** Functions */
+    // @Order recieve, fallback, external, public, internal, private
+    receive() external payable{}
+    fallback() external payable{}
+
+
+    /** Getter Functions */
+
+}
